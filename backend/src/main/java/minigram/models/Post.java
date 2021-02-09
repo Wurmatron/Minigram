caa47bfe6c43c0b599a21c7e1071982e8be7e1e5
Tangeni Shikomba <tangenishikomba@gmail.com>
2021-02-09 12:58:33 -0500

    Added the post model

---
+++ backend/src/main/java/minigram/models/Post.java
@@ -0,0 +1,15 @@
+package minigram.models;
+
+import minigram.utils.wrapper.IModel;
+
+public class Post implements IModel {
+
+//    TODO Add all the fields
+    public String id;
+
+
+    public Post(String id) {
+        this.id = id;
+
+    }
+}
