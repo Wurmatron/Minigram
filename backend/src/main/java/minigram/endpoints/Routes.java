ace0d10b5e50d198627ebeaad851fc3f0f19becf
Tangeni Shikomba <tangenishikomba@gmail.com>
2021-02-11 10:02:25 -0500

    routes files created

--- backend/src/main/java/minigram/endpoints/Routes.java
+++ backend/src/main/java/minigram/endpoints/Routes.java
@@ -2,16 +2,16 @@ package minigram.endpoints;
 
 import io.javalin.Javalin;
 import minigram.controllers.AccountController;
-import minigram.controllers.AuthController;
 import minigram.utils.anotations.Endpoint;
 
-public class AccountRoutes {
+public class Routes {
 
     @Endpoint
     public void authentication(Javalin app) {
         app.post("account/register", AccountController.registerNewAccount);
 //        app.post("login", AuthController.login);
 //        app.post("logout", AuthController.logout);
+//        app.post("reset-password", AuthController.reset_password);
     }
 
     @Endpoint
