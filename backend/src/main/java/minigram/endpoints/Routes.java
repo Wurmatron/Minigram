600a75b0ac762f6bf30f5b26c0dfc8f59fcbc03f
Tangeni Shikomba <tangenishikomba@gmail.com>
2021-02-14 22:28:15 -0500

    auth routes added

--- backend/src/main/java/minigram/endpoints/Routes.java
+++ backend/src/main/java/minigram/endpoints/Routes.java
@@ -2,6 +2,7 @@ package minigram.endpoints;
 
 import io.javalin.Javalin;
 import minigram.controllers.AccountController;
+import minigram.controllers.AuthController;
 import minigram.utils.anotations.Endpoint;
 
 public class Routes {
@@ -9,8 +10,8 @@ public class Routes {
     @Endpoint
     public void authentication(Javalin app) {
         app.post("account/register", AccountController.registerNewAccount);
-//        app.post("login", AuthController.login);
-//        app.post("logout", AuthController.logout);
+        app.post("login", AuthController.login);
+        app.post("logout", AuthController.logout);
 //        app.post("reset-password", AuthController.reset_password);
     }
 
