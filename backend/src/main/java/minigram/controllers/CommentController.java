6edbb1809401d695b5959ee34068145143b5a69f
Tangeni Shikomba <tangenishikomba@gmail.com>
2021-02-16 11:23:46 -0500

    New Controllers added

---
+++ backend/src/main/java/minigram/controllers/CommentController.java
@@ -0,0 +1,15 @@
+package minigram.controllers;
+
+import io.javalin.http.Handler;
+
+public class CommentController {
+
+    public void CommentController() {
+
+    }
+
+//    TODO: Implement
+    public static Handler  fetchPostComments = ctx -> {
+
+    };
+}
---
+++ backend/src/main/java/minigram/controllers/PostsController.java
@@ -0,0 +1,31 @@
+package minigram.controllers;
+
+import io.javalin.http.Handler;
+
+public class PostsController {
+
+    public void PostController() {
+
+    }
+
+    //    TODO: Implement
+    public static Handler fetchPosts = ctx -> {
+
+    };
+
+    //    TODO: Implement
+    public static Handler fetchPost = ctx -> {
+
+    };
+
+    //    TODO: Implement
+    public static Handler updatePost = ctx -> {
+
+    };
+
+    //    TODO: Implement
+    public static Handler deletePost = ctx -> {
+
+    };
+
+}
