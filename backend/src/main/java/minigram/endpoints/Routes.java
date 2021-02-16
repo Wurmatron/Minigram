b2cbb01cb41b4bb898f9e186428456ecf33b47be
Tangeni Shikomba <tangenishikomba@gmail.com>
2021-02-16 11:35:30 -0500

    Routes.java updated

--- backend/src/main/java/minigram/endpoints/Routes.java
+++ backend/src/main/java/minigram/endpoints/Routes.java
@@ -3,6 +3,8 @@ package minigram.endpoints;
 import io.javalin.Javalin;
 import minigram.controllers.AccountController;
 import minigram.controllers.AuthController;
+import minigram.controllers.CommentController;
+import minigram.controllers.PostsController;
 import minigram.utils.anotations.Endpoint;
 
 public class Routes {
@@ -21,5 +23,18 @@ public class Routes {
         app.get("/account", AccountController.fetchAccounts);
         app.put("/account", AccountController.updateAccount);
 //        app.delete("/account/delete", AccountController.deleteAccount);
+    }
+
+    @Endpoint
+    public void post(Javalin app){
+//        app.get("/posts/:id", PostsController.fetchPost);
+//        app.get("/posts/", PostsController.fetchPosts);
+//        app.put("/posts/:id", PostsController.updatePost);
+//        app.delete("/posts/:id", PostsController.deletePost);
+    }
+
+    @Endpoint
+    public void postComments(Javalin app){
+//        app.get("/posts/:id/comments", CommentController.fetchPostComments);
     }
 }
